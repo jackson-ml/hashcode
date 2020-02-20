730e300dfb7cbe0e5e427912da1affa307a044ee
Jackson <jmeyerlee@haverford.edu>
2020-02-20 14:11:00 -0500

    give libraries id's

--- parse_data.py
+++ parse_data.py
@@ -4,10 +4,15 @@ class Library:
     def __len__(self):
         len(self.books)
 
-    def __init__(self, books, signup, rate):
+    def __str__(self):
+        return id
+
+    def __init__(self, id, books, signup, rate):
+        self.id = id
         self.books = books
         self.signup = signup
         self.rate = rate
+
 
 def parse_input_file(input):
     with open(input) as input_file:
@@ -22,7 +27,7 @@ def parse_input_file(input):
         for i in range(l):
             size_signup_and_rate = [int(i) for i in input_file.readline().split()]
             books = [int(i) for i in input_file.readline().split()]
-            library = Library(books, size_signup_and_rate[1],
+            library = Library(i+1, books, size_signup_and_rate[1],
                               size_signup_and_rate[2])
             libraries.append(library)
         return b, l, d, book_scores, libraries
